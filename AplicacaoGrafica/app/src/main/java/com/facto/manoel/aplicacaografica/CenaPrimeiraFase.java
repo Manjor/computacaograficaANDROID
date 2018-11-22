f6af65e3d022acfc4a452b39300b0805674c34e8
Manjor <manoeltavares.jr@gmail.com>
2018-11-21 23:22:44 -0200

    Incluso a colisão da bala e o respawn do cachorro

--- AplicacaoGrafica/app/src/main/java/com/facto/manoel/aplicacaografica/CenaPrimeiraFase.java
+++ AplicacaoGrafica/app/src/main/java/com/facto/manoel/aplicacaografica/CenaPrimeiraFase.java
@@ -1,9 +1,12 @@
 package com.facto.manoel.aplicacaografica;
 
 import com.facto.manoel.aplicacaografica.AndGraph.AGGameManager;
+import com.facto.manoel.aplicacaografica.AndGraph.AGInputManager;
 import com.facto.manoel.aplicacaografica.AndGraph.AGScene;
 import com.facto.manoel.aplicacaografica.AndGraph.AGScreenManager;
+import com.facto.manoel.aplicacaografica.AndGraph.AGSoundManager;
 import com.facto.manoel.aplicacaografica.AndGraph.AGSprite;
+import com.facto.manoel.aplicacaografica.AndGraph.AGTimer;
 
 public class CenaPrimeiraFase extends AGScene {
 
@@ -26,6 +29,13 @@ public class CenaPrimeiraFase extends AGScene {
     AGSprite morendo = null;
     AGSprite abaixando = null;
     AGSprite cachorrorun = null;
+    AGSprite bala = null;
+    float balaX = 0;
+    float cachorroX = 0;
+    float balaposInicial = 0;
+    float cachorroposInicial = 0;
+    AGTimer tempoRespawCachorro = null;
+
     public CenaPrimeiraFase(AGGameManager pManager) {
         super(pManager);
         this.agGameManager = pManager;
@@ -33,6 +43,8 @@ public class CenaPrimeiraFase extends AGScene {
 
     @Override
     public void init() {
+
+        this.tempoRespawCachorro =  new AGTimer(2000);
         setSceneBackgroundColor(0.2f,0.2f,0.4f);
         this.background = createSprite(R.mipmap.inferno,1,1);
         this.background.setScreenPercent(100,100);
@@ -75,14 +87,21 @@ public class CenaPrimeiraFase extends AGScene {
 //        this.abaixando.vrPosition.setY(AGScreenManager.iScreenHeight/2);
 //        this.abaixando.addAnimation(5,true,31,32);
 
-        this.cachorrorun = createSprite(R.mipmap.hellrun, 5,1);
+        this.cachorrorun = createSprite(R.mipmap.hellway, 12,1);
         this.cachorrorun.setScreenPercent(20,25);
         this.cachorrorun.vrPosition.setX(AGScreenManager.iScreenWidth * 0.85f);
         this.cachorrorun.vrPosition.setY(AGScreenManager.iScreenHeight * 0.10f);
+        this.cachorroposInicial = cachorrorun.vrPosition.getX();
 
-        this.cachorrorun.addAnimation(5,true,0,4);
+        this.cachorrorun.addAnimation(10,true,0,11);
+        this.bala = createSprite(R.mipmap.bullet, 1,1);
+        this.bala.setScreenPercent(5,10);
+        this.bala.vrPosition.setX(parado.vrPosition.getX() + (parado.getSpriteWidth() / 2) + (bala.getSpriteWidth() / 3));
+        this.bala.vrPosition.setY(parado.vrPosition.getY() + (parado.getSpriteHeight() / 6));
 
-
+        this.balaposInicial =  bala.vrPosition.getX();
+        this.bala.bVisible = false;
+        //this.cachorrorun.addAnimation(10,true,0,11);
     }
 
     @Override
@@ -94,9 +113,52 @@ public class CenaPrimeiraFase extends AGScene {
     public void stop() {
 
     }
-
+    int i = 0;
+    int j = 0;
     @Override
     public void loop() {
 
+        if(bala.vrPosition.getX() >= (AGScreenManager.iScreenWidth + 20)) {
+            i = 0;
+            balaX = 0;
+            bala.bVisible = false;
+            this.bala.vrPosition.setX(this.balaposInicial);
+        }
+        if(AGInputManager.vrTouchEvents.screenClicked() && bala.bVisible == false ){
+            bala.bVisible = true;
+            i = 2;
+            balaX = 0;
+        }
+        if(bala.collide(cachorrorun) && bala.bVisible == true){
+            this.cachorrorun.bVisible = false;
+            this.bala.bVisible = false;
+            this.cachorrorun.vrPosition.setX(this.cachorroposInicial);
+            this.j = 0;
+            cachorroX = 0;
+        }
+
+        if(cachorrorun.bVisible == false){
+            this.tempoRespawCachorro.update();
+            if(this.tempoRespawCachorro.isTimeEnded()){
+                this.cachorrorun.bVisible = true;
+                this.tempoRespawCachorro.restart();
+            }
+        }
+        if(cachorrorun.bVisible == true){
+            j = 2;
+        }
+        if(cachorrorun.collide(parado)){
+
+        }
+        if(this.cachorrorun.vrPosition.getX() <= (-20)){
+            this.cachorrorun.bVisible = false;
+            this.cachorrorun.vrPosition.setX(this.cachorroposInicial);
+            this.j = 0;
+            this.cachorroX = 0;
+        }
+        cachorroX += j;
+        balaX+= i;
+        bala.vrPosition.setX(bala.vrPosition.getX() + balaX);
+        cachorrorun.vrPosition.setX(cachorrorun.vrPosition.getX() - cachorroX);
     }
 }
